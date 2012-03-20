e10b5dbbb3994b6e7c343ab4131bbc7142b1ed06
Andrey Samoylov <a.samoylov@svadom.ru>
2012-03-20 19:26:54 +0400

    new index page and general styles

--- application/views/users_interface/footer.php
+++ application/views/users_interface/footer.php
@@ -1,3 +1,18 @@
-<footer>
-	<p>&copy; Российский Центр Дополнительного Профессионального Обучения 2012</p>
-</footer>
+<div class="container">
+	<footer>
+		<div class="row">
+			<div class="span5">
+				<div class="copy">&copy; Образовательный портал АНО ДПО 2012</div>
+			</div>
+			<div class="span4">
+				г.Ростов-на-Дону, ул.Строителей, 75 оф.412
+			</div>
+			<div class="span3">
+				<div id="bt-contacts">
+					<span class="desc">Тел.:</span>
+					<b>(863)</b> 295-52-10
+				</div>
+			</div>			
+		</div>
+	</footer>	
+</div>
--- application/views/users_interface/index.php
+++ application/views/users_interface/index.php
@@ -1,57 +1,117 @@
 <!DOCTYPE html>
 <html lang="en">
-<?php $this->load->view('users_interface/head');?>
+<? $this->load->view('users_interface/head');?>
 <body>
-	<?php $this->load->view('users_interface/header');?>
+	<? $this->load->view('users_interface/header');?>
 	<div class="container">
 		<div class="row">
-			<div class="span9">
-				<div class="hero-unit">
-					<h2>РосЦентр ДПО</h2>
-					<p>Система дистанционного обучения позволяет организовать образовательный процесс с помощью электронных учебных курсов в дистанционной форме через Интернет.</p>
-					<p><a class="btn btn-primary btn-large">Узнать больше &raquo;</a></p>
-				</div>
-				<div class="row">
-					<div class="span3">
-						<h2>Об академии</h2>
-						<p>Российский Центр Дополнительного Профессионального Обучения является государственным образовательным учреждением дополнительного профессионального образования.</p>
-						<p>Академия учреждена в 2012 году Распоряжением Правительства Российской Федерации от 21.02.2012 №709-р и находится в ведении Министерства регионального развития России.</p>
-						<p><a class="btn" href="#">Подробнее &raquo;</a></p>
-					</div>
-					<div class="span3">
-						<h2>Документы</h2>
-						<p>Документ о повышении квалификации – Удостоверение государственного образца. Документы заказа (счёт и договор) заказчик получает автоматически, по завершению оформления заявки.</p>
-						<p>Все документы заказа можно получить в оригинале – для этого обращайтесь к консультантам.</p>
-						<p><a class="btn" href="#">Подробнее &raquo;</a></p>
-					</div>
-					<div class="span3">
-						<h2>СРО</h2>
-						<p>Центр предлагает СРО строителей, проектировщиков, изыскателей и энергоаудиторов заключить Соглашение о сотрудничестве.</p>
-						<p><a class="btn" href="#">Подробнее &raquo;</a></p>
-					</div>
-				</div>
+			<div id="promo-banner" class="span12">
+				<h1>Южно-окружной центр повышения квалификации и переподготовки кадров <br />для строительного и жилищно-коммунального комплекса</h1>
+				<h3>Повышение квалификации через интернет с выдачей удостоверения государственного образца.</h3>
 			</div>
-		<?php if(!$loginstatus['status']):?>
-			<?php $this->load->view('users_interface/rightbarauth');?>
-		<?php endif;?>
-		<?php if($loginstatus['status'] && $loginstatus['cus']):?>
-			<?php $this->load->view('users_interface/rightbarcus');?>
-		<?php endif;?>
-		<?php if($loginstatus['status'] && $loginstatus['aud']):?>
-			<?php $this->load->view('users_interface/rightbaraud');?>
-		<?php endif;?>
-		<?php if($loginstatus['status'] && $loginstatus['adm']):?>
-			<?php $this->load->view('users_interface/rightbaradm');?>
-		<?php endif;?>
 		</div>
-		<hr>
-	<?php $this->load->view('users_interface/footer');?>	
+		<div class="row">
+			<div class="span9">
+				<h2>Система дистанционного образования</h2>
+				<p>
+					Образовательный портал АНО ДПО «Южно-окружной центр повышения квалификации и переподготовки кадров 
+					для строительного и жилищно-коммунального комплекса» предлагает курсы дистанционного дополнительного 
+					профессионального образования в сфере строительства, проектирования, инженерных изысканий, 
+					коммунального хозяйства и энергетического менеджмента.
+				</p>
+				<p>
+					Система дистанционного обучения позволяет организовать образовательный процесс с помощью электронных 
+					учебных курсов в дистанционной форме через Интернет. После окончания обучения и успешной сдачи итогового 
+					тестирования слушатели курсов получают удостоверения государственного образца.
+				</p>
+			</div>
+			<div class="span3">
+				<h4>Последние добавленые курсы</h4>
+				<ul id="courses-new">
+					<li><a href="#">БС-12. Безопасность строительства и качество выполнения геодезических, подготовительных и земляных работ</a></li>
+					<li><a href="#">БС-14. Безопасность строительства и качество возведения бетонных и железобетонных строительных </a></li>
+					<li><a class="details" href="#">Полный список курсов &raquo;</a></li>
+				</ul>
+			</div>
+		</div>
+		<div class="row">
+			<div class="span12">
+				<h2 class="bordered">Этапы прохождения обучения</h2>
+			</div>
+			<div class="span3">
+				<p class="caption">1. Оформление заявки <span class="next">&gt;</span></p>
+				<p>
+					1. Зарегистрируйте заказчика; <br />
+					2. Выберите курсы; <br />
+					3. Зарегистрируйте слушателей; <br />
+					4. Получите счет и договор.
+				</p>
+			</div>
+			<div class="span3">
+				<p class="caption">2. Оплата <span class="next">&gt;</span></p>
+				<p>
+					Счет Вы получите автоматически по завершению оформления заявки. Совершить оплату 
+					Вы можете в любом банке. Доступ к лекциям открывается в течении 1-2 дней после оплаты. 
+				</p>
+			</div>
+			<div class="span3">
+				<p class="caption">3. Обучение <span class="next">&gt;</span></p>
+				<p>
+					Слушатели обучаются в своих личных кабинетах на сайте. Для завершения обучения 
+					слушатели выполняют итоговое тестирование.  
+				</p>
+			</div>
+			<div class="span3">
+				<p class="caption">4. Получение удостоверения</p>
+				<p>
+					Как только слушатели успешно завершают обучение, мы выдаем государственные удостоверения о 
+					повышении квалификации. Документы можно забрать лично или получить экспресс-почтой.  
+				</p>
+			</div>
+		</div>
 	</div>
-	<?php $this->load->view('users_interface/scripts');?>
+	<!--
+	<div class="container">
+		<div class="row">
+			<div class="span12">
+				<? 
+				if(!$loginstatus['status']):
+					$this->load->view('users_interface/rightbarauth');
+				endif;
+				if($loginstatus['status'] && $loginstatus['cus']):
+					$this->load->view('users_interface/rightbarcus');
+				endif;
+				if($loginstatus['status'] && $loginstatus['aud']):
+					$this->load->view('users_interface/rightbaraud');
+				endif;
+				if($loginstatus['status'] && $loginstatus['adm']):
+					$this->load->view('users_interface/rightbaradm');
+				endif;
+				?>
+			</div>
+		</div>
+	</div>
+	-->
+	<? $this->load->view('users_interface/footer');?>
+	<? $this->load->view('users_interface/scripts');?>
 	<script type="text/javascript">
-		$(document).ready(function(){
-			$("#lsend").click(function(event){var err = false;$(".help-inline").hide();$(".focused").each(function(i,element){if($(this).val()==''){$(this).siblings(".help-inline").html('<i class="icon-exclamation-sign" title="Поле не может быть пустым"></i>').show();err = true;}});if(err){event.preventDefault()};});
-			$("#authCancel").click(function(){$(".help-inline").html('').hide();});
+		$(document).ready(function() {
+			$("#lsend").click(function(event) {
+				var err = false;
+				$(".help-inline").hide();
+				$(".focused").each(function(i, element) {
+					if($(this).val() == '') {
+						$(this).siblings(".help-inline").html('<i class="icon-exclamation-sign" title="Поле не может быть пустым"></i>').show();
+						err = true;
+					}
+				});
+				if(err) {
+					event.preventDefault()
+				};
+			});
+			$("#authCancel").click(function() {
+				$(".help-inline").html('').hide();
+			});
 		});
 	</script>
 </body>
