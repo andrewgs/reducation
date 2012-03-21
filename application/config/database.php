9bc0b0637cb208b80df710435ebecb07c895d960
Andrey Samoylov <a.samoylov@svadom.ru>
2012-03-21 12:26:40 +0400

    none

--- application/config/database.php
+++ application/config/database.php
@@ -4,7 +4,7 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = "localhost";
-//$db['default']['port'] 	   = 3307;
+$db['default']['port'] 	   = 3307;
 $db['default']['username'] = "root";
 $db['default']['password'] ="{bvthfVecrkm57";
 $db['default']['database'] = "reducation";
