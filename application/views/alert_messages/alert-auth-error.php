2f689c3a72ea56313cd095bb40b04e732e314bcb
Vladimir <vkharseev@gmail.com>
2012-03-27 02:23:38 +0400

    27032012-1

--- application/views/alert_messages/alert-auth-error.php
+++ application/views/alert_messages/alert-auth-error.php
@@ -1,7 +1,6 @@
 <?php if($msgauth):?>
 	<div class="alert alert-error" id="msgdealert">
 		<a class="close" id="msgeclose">×</a>
-		<h4 class="alert-heading">Внимание!</h4>
 		<?=$msgauth;?>
 	</div>
 <?php endif; ?>
