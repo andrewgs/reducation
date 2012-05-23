075c44e99ddd065f259c95958fef763f0e74770f
Vladimir <vkharseev@gmail.com>
2012-05-23 15:28:41 +0400

    23052012-2

--- application/views/admin_interface/test-report-short.php
+++ application/views/admin_interface/test-report-short.php
@@ -2,26 +2,47 @@
 <html lang="en">
 <?php $this->load->view('audience_interface/head');?>
 <body>
-	<div class="container">
+	<style type="text/css">
+		@media print {
+			body, p { font-family: Tahoma, sans-serif; font-size: 16px; line-height: 24px; margin-bottom: 14px; }
+		}
+	</style>
+	<div class="container-fluid" style="position: relative;">
 		<div class="row">
-			<div class="span9">
-				<pre>
-<b>Организатор тестирования (физическое/юридическое лицо): </b><br/><u>АНО ДПО "Южно-окружной центр повышения квалификации"</u>
-
-<b>Дата тестирования: </b><?=$info['dateover'];?>
-
-<b>Ф.И.О.: </b><u><?=$info['lastname'].' '.$info['name'].' '.$info['middlename'];?></u>
-<b>Занимаемая должность: </b><?=$info['position'];?>
-
-<b>Место работы (наименование организации): </b><?=$info['organization'];?>
-
-<b>Email: </b><a href="mailto:<?=$info['personaemail'];?>" target="_blank"><?=$info['personaemail'];?></a>
-<b>Наименование программы: </b><?=$info['ccode'].' '.$info['ctitle'];?>
-
-<b>Наименование теста: </b><?=$info['ttitle'];?>
-
-<b>Результат итогового тестирования: </b><?=$info['result'];?>%
-				</pre>
+			<div class="span12">
+           		<p><strong>Организатор тестирования (физическое/юридическое лицо):<br/><u>АНО ДПО "Южно-окружной центр повышения квалификации"</u></strong></p>
+				<table class="table">
+					<tbody>
+						<tr>
+							<td>Дата тестирования:</td>
+							<td><?=$info['dateover'];?></td>
+						</tr>
+						<tr>
+							<td>Ф.И.О.:</td>
+							<td><?=$info['lastname'].' '.$info['name'].' '.$info['middlename'];?></td>
+						</tr>
+						<tr>
+							<td>Занимаемая должность: </td>
+							<td><?=$info['position'];?></td>
+						</tr>
+						<tr>
+							<td>Место работы (наименование организации): </td>
+							<td><?=$info['organization'];?></td>
+						</tr>
+						<tr>
+							<td>Email: </td>
+							<td><a href="mailto:<?=$info['personaemail'];?>" target="_blank"><?=$info['personaemail'];?></a></td>
+						</tr>
+						<tr>
+							<td>Наименование программы:</td>
+							<td><?=$info['ccode'].' '.$info['ctitle'];?></td>
+						</tr>
+						<tr>
+							<td>Результат итогового тестирования: </td>
+							<td><?=$info['result'];?>%</td>
+						</tr>
+					</tbody>
+				</table>
 			</div>
 		</div>
 	</div>
