5df4b7664e84f9522d6f7a942e281d336dffb5a8
Andrey <andrew.samoilov@gmail.com>
2012-10-10 01:15:42 +0400

    banch from the server

--- application/views/customer_interface/customer-order-invoice.php
+++ application/views/customer_interface/customer-order-invoice.php
@@ -98,16 +98,16 @@
 				<p style="margin: 80px 0 0;">
 					<strong>В платежном поручении в графе «назначение платежа» обязательно должно быть указано <i>«Оплата за Повышение квалификации по Договору №<?=$order['id'];?> от <?=$order['orderdate'];?> по счету № <?=$order['id'];?> от <?=$order['orderdate'];?> НДС не облагается»</i></strong>
 				</p>
-				<div id="#buhgl" style="position: absolute; bottom: 100px; left: 220px;">
+				<div id="#pechat" style="position: absolute; bottom: 70px; left: 270px;">
+					<img src="<?=base_url()?>img/pechat.png"/>
+				</div>
+				<div id="#buhgl" style="position: absolute; bottom: 75px; left: 220px;">
 					<img src="<?=base_url()?>img/buhgl.png"/>
 				</div>
 				<div id="#klimova" style="position: absolute; bottom: 144px; left: 220px;">
 					<img src="<?=base_url()?>img/klimova.png"/>
-				</div>				
+				</div>
 			</div>
-		</div>
-		<div id="#pechat" style="position: absolute; bottom: 70px; left: 270px;">
-			<img src="<?=base_url()?>img/pechat.png"/>
 		</div>
 	</div>
 </body>
