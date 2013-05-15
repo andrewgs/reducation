06593ba45a5c72a7334c0a73ba7706a96b89e0c7
Vladimir Kharseev <vkharseev@gmail.com>
2013-05-15 19:14:43 +0400

    15.05.2013 19:15

--- application/controllers/crontab_interface.php
+++ application/controllers/crontab_interface.php
@@ -19,17 +19,27 @@ class Crontab_interface extends MY_Controller{
 <p>
 	Система дистанционного обучения АНО ДПО «Южно-окружной центр повышения квалификации» <a href="http://roscentrdpo.ru/">http://roscentrdpo.ru/</a> 
 	напоминает: 
-	<h4>Ваш период сдачи итогового тестированя по заказу №<?=$customers[$i]['number'].'/'.$customers[$i]['year'];?> от <?=swap_dot_date($customers[$i]['orderdate']);?><br/> с <?=swap_dot_date($customers[$i]['closedate']);?> по <?=date("d.m.Y",future_days(10,$customers[$i]['closedate']));?></h4>
+	<h4>Ваш период сдачи итогового тестированя по заказу №<?=$customers[$i]['number'].'/'.$customers[$i]['year'];?> от <?=swap_dot_date($customers[$i]['orderdate']);?><br/>
+	 наступает с <?=swap_dot_date($customers[$i]['closedate']);?> по <?=date("d.m.Y",future_days(10,$customers[$i]['closedate']));?></h4>
 </p>
 <br/><br/>
 <p>
-	Наш адрес: г.Ростов-на-Дону, ул.Республиканская, д.86<br/>
-	Контактные данные: Тел.:(863) 246-43-54 Эл.почта: info@roscentrdpo.ru<br/>
-	С уважением, Администрация Образовательного портала АНО ДПО «Южно-окружной центр повышения квалификации»
+	Наш адрес:<br/>
+	г.Ростов-на-Дону, ул.Республиканская, д.86<br/>
+	Тел.:(863) 246-43-54<br/>
+	Эл.почта: info@roscentrdpo.ru<br/>
+	<br/>С уважением,<br/>Администрация Образовательного портала<br/>АНО ДПО «Южно-окружной центр повышения квалификации»
 </p>
 		<?php
-		$mailtext = ob_get_clean();
-		$this->sendMail($customers[$i]['personemail'],'admin@roscentrdpo.ru','АНО ДПО','Уведомление о итоговом тестировании',$mailtext);
+			$mailtext = ob_get_clean();
+			if($this->input->get('mode') == 'test'):
+				echo $customers[$i]['personemail']."<br/>--------------------------<br/>";
+				echo 'Текст письма:<br/>';
+				echo $mailtext;
+				echo '<br/>';
+			else:
+				$this->sendMail($customers[$i]['personemail'],'admin@roscentrdpo.ru','АНО ДПО','Уведомление о итоговом тестировании',$mailtext);
+			endif;
 		endfor;
 		echo 'OK';
 	}
