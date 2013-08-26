d6815eed28f3dae7ffb742cf2e8f67db95afe489
Vladimir Kharseev <vkharseev@gmail.com>
2013-08-26 14:31:24 +0400

    26/08/13

--- application/views/physical_interface/profile.php
+++ application/views/physical_interface/profile.php
@@ -29,7 +29,6 @@
 		<?php endif;?>
 			$("#save").click(function(event){var err = false; var email = $("#email").val(); $(".control-group").removeClass('error');$(".help-inline").hide();$(".inpval").each(function(i,element){if($(this).val()==''){$(this).parents(".control-group").addClass('error');$(this).siblings(".help-inline").html("Поле не может быть пустым").show();err = true;}});if(err){event.preventDefault();};if(!err && !isValidEmailAddress(email)){$("#email").addClass('error');$("#email").html("Не верный адрес E-Mail").show();event.preventDefault();}});
 			$(".digital").keypress(function(e){if(e.which!=8 && e.which!=46 && e.which!=0 && (e.which<48 || e.which>57)){return false;}});
-			$("#accounttype").val("<?=$physical['accounttype'];?>");
 			function isValidEmailAddress(emailAddress){var pattern = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);return pattern.test(emailAddress);};
 		});
 	</script>
